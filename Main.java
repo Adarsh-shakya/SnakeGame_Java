76bdb9e4381bcee8e4266daff8704518ddda329f
Adarsh-shakya <adarsh.shakya_cs20@gla.ac.in>
2022-02-10 22:48:12 +0530

    main class

---
+++ Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame=new JFrame("Snake Game");
+        frame.setBounds(200,10,905,700);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        GamePanel panel=new GamePanel();
+        panel.setBackground(Color.DARK_GRAY);
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+}
